1087fd597575a52a86cf647edcfecc72a0dc30ce
yuri <example@gmail.com>
2022-07-24 12:14:44 +0400

    first lesson

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let person = {
+    name: "John",
+    age: "25",
+    isMarried: false
+};
+
+//console.log(person["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[2]);
+
+//alert("Hello World!")
+
+/*let answer = confirm("Are you here?");
+
+console.log(answer);*/
+
+
+let answer = prompt("Есть ли вам 18?", "Да");
+
+console.log(typeof(arr));
